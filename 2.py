74cb17afbe49e2f5da519f023a1cfc556492da07
Joshua Equere <marveloper.8@gmail.com>
2022-01-09 17:03:04 +0100

    uniform distribution - matplot

--- 2.py
+++ 2.py
@@ -72,10 +72,12 @@ from numpy import random
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.distplot(random.binomial(n=1000, p=0.01, size=1000), hist=False, label='binomial')
-sns.distplot(random.poisson(lam=10, size=1000), hist=False, label='poisson')
+sns.distplot(random.uniform(size=1000), hist=False)
 
 plt.show()
+
+# sns.distplot(random.binomial(n=1000, p=0.01, size=1000), hist=False, label='binomial')
+# sns.distplot(random.poisson(lam=10, size=1000), hist=False, label='poisson')
 
 # sns.distplot(random.normal(loc=50, scale=7, size=1000), hist=False, label='normal')
 # sns.distplot(random.poisson(lam=50, size=1000), hist=False, label='poisson')
