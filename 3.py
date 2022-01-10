90aa9fe58c2bd42eaf206f9339f0a199198ab81c
Joshua Equere <marveloper.8@gmail.com>
2022-01-10 22:18:29 +0100

    fix

--- 3.py
+++ 3.py
@@ -68,3 +68,12 @@ import numpy as np
 
 a = np.trunc([-3.1666, 3.6667])
 print(a)
+
+b = np.fix([-3.1666, 3.6667])
+print(b)
+
+# c = np.around(3.1666, 2)
+# print(c)
+
+# d = np.floor([-3.1666, 3.6667])
+# print(d)
