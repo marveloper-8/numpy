ff271bda02e607ffd4664a1ddf2f3c67d1b96cd7
Joshua Equere <marveloper.8@gmail.com>
2022-01-10 20:12:44 +0100

    create your own ufunc

--- 3.py
+++ 3.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+#normal python ufunc
 a = [1, 2, 3, 4]
 b = [4, 5, 6, 7]
 c = []
@@ -6,7 +9,22 @@ for i, j in zip(a, b):
     c.append(i + j)
 print(c)
 
-import numpy as np
-
+#numpy ufunc
 d = np.add(a, b)
 print(d)
+
+#custom numpy ufunc
+def myadd(e, f):
+    return e + f
+
+myadd = np.frompyfunc(myadd, 2, 1)
+
+print(myadd([1, 2, 3, 4], [5, 6, 7, 8]))
+
+print(type(np.add))
+print(type(np.concatenate))
+
+if type(np.add) == np.ufunc:
+    print('add is ufunc')
+else:
+    print('add is not ufunc')
