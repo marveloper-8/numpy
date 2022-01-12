6940958d1c863558812b1d1f0ca4fab29e4c555b
Joshua Equere <marveloper.8@gmail.com>
2022-01-12 18:32:18 +0100

    key/value objects as series

--- 4.py
+++ 4.py
@@ -12,7 +12,17 @@
 import pandas as pd
 a = [1, 7, 2]
 b = pd.Series(a)
+print(b)
 print(b[0])
 
 c = pd.Series(a, index=["x", "y", "z"])
 print(c)
+print(c["y"])
+
+d = {
+    "day1": 420,
+    "day2": 380,
+    "day3": 390
+}
+e = pd.Series(d)
+print(e)
